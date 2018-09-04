fb99b3501c3a463df459b8cd36225cc4212d67e9
Paul Michael John Lopez <pmjlopez@gmail.com>
2018-09-04 13:42:29 +0800

    refactor: handle receive users

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,25 @@
+import { getUsers } from '../utils/api'
+import { showLoading, hideLoading } from 'react-redux-loading'
+
 export const RECEIVE_USERS = 'RECEIVE_USERS'
 
 export function receiveUsers (users) {
     return {
         type: RECEIVE_USERS,
         users
+    }
+}
+
+export function handleReceiveUsers () {
+    return (dispatch) => {
+        dispatch(showLoading())
+        return getUsers()
+            .then((users) => dispatch(receiveUsers(users)))
+            .then(() => dispatch(hideLoading()))
+            .catch((e) => {
+                console.warn('There was an error in handleReceiveUsers. Try again', e)
+                alert(`There was an error in receiving users. Try again.`)
+                dispatch(hideLoading())
+            })
     }
 }
