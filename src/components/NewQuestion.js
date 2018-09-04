6929815baa1cc03981bddeee26fbab399f2fd0b0
Paul Michael John Lopez <pmjlopez@gmail.com>
2018-09-04 13:35:52 +0800

    feat: add new question component

---
+++ src/components/NewQuestion.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleAddQuestion } from "../actions/questions"
+
+class NewQuestion extends Component {
+    state = {
+        optionOneText: '',
+        optionTwoText: ''
+    }
+    handleChangeText = (e) => {
+        const text = e.target.value
+        const option = e.target.name
+        console.log('Option/Text', option, text)
+        this.setState(() => ({
+            [option]: text
+        }))
+    }
+    handleAddQuestion = (e) => {
+        e.preventDefault()
+        const { dispatch } = this.props
+        const { optionOneText, optionTwoText } = this.state
+        dispatch(handleAddQuestion(
+            optionOneText,
+            optionTwoText
+        ))
+    }
+    render() {
+        return (
+            <form className='card' onSubmit={this.handleAddQuestion}>
+                <div className='card-header'>
+                    Create New Question
+                </div>
+                <div className='card-body'>
+                    <p className='card-text'>Complete the question:</p>
+                    <h5 className='card-title'>Would you rather...</h5>
+                    <input
+                        className='form-control'
+                        type='text'
+                        name='optionOneText'
+                        onChange={this.handleChangeText}
+                        placeholder='Enter Option One Text Here'
+                    />
+                    <div className='card-text text-center'>OR</div>
+                    <input
+                        className='form-control'
+                        type='text'
+                        name='optionTwoText'
+                        onChange={this.handleChangeText}
+                        placeholder='Enter Option Two Text Here'
+                    />
+                </div>
+                <div className='card-footer text-center'>
+                    <button type='submit' className='btn btn-primary'>Submit</button>
+                </div>
+            </form>
+        );
+    }
+}
+
+function mapStateToProps ({authedUser}) {
+    return {
+        authedUser
+    }
+}
+
+export default connect(mapStateToProps)(NewQuestion)
