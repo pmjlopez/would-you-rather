f5351821f725e0bcbbaaccebe88ccf1f7dc93aa3
Paul Michael John Lopez <pmjlopez@gmail.com>
2018-09-04 13:36:49 +0800

    feat: add login and logout

---
+++ src/components/Login.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleSetAuthedUser } from "../actions/authedUser";
+
+class Login extends Component {
+    handleChangeUser = (e) => {
+        const { dispatch } = this.props
+        const id = e.target.value
+        dispatch(handleSetAuthedUser(id))
+    }
+    render() {
+        const { userIds, users } = this.props
+        return (
+            <div className='container text-center'>
+                <h5>Login</h5>
+                <select onChange={this.handleChangeUser}>
+                    <option value=''>Select User</option>
+                    {userIds.map((id) => (
+                        <option key={id} value={id}>{users[id].name}</option>
+                    ))}
+                </select>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps ({users}) {
+    return {
+        userIds: Object.keys(users),
+        users
+    }
+}
+
+export default connect(mapStateToProps)(Login)
