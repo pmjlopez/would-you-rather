341ab28389476224824816ecf8be82070a75082a
Paul Michael John Lopez <pmjlopez@gmail.com>
2018-09-04 13:37:21 +0800

    feat: add styling to navigation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,58 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { handleUnsetAuthedUser } from "../actions/authedUser"
 
 class Nav extends Component {
+    handleLogout = (e) => {
+        const { dispatch } = this.props
+        dispatch(handleUnsetAuthedUser())
+    }
     render() {
+        const { user } = this.props
         return (
-            <div>
-                <a href='/'>Home</a>
-                <a href='/new'>New Question</a>
-                <a href='/leader-board'>Leader Board</a>
-            </div>
+            <nav className="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
+                <button className="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <span className="navbar-brand" href="#">
+                    Hello, {user.name}
+                    <img src={user.avatarURL} width="30" height="30"
+                         className="d-inline-block align-top" alt={user.name}/>
+                </span>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <ul className='navbar-nav mr-auto'>
+                        <li className='nav-item'>
+                            <NavLink className='nav-link' to='/'>Home</NavLink>
+                        </li>
+                        <li className='nav-item'>
+                            <NavLink className='nav-link' to='/new'>New Question</NavLink>
+                        </li>
+                        <li className='nav-item'>
+                            <NavLink className='nav-link' to='/leader-board'>Leader Board</NavLink>
+                        </li>
+                        <li className='nav-item'>
+                            <a
+                                className='nav-link'
+                                onClick={this.handleLogout}
+                            >
+                                Logout
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         );
     }
 }
 
-export default Nav
+function mapStateToProps ({users, authedUser}) {
+    const user = users[authedUser]
+    return {
+        user
+    }
+}
+
+export default connect(mapStateToProps)(Nav)
