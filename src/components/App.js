96b93c0b528ca8ecca03e9ba2dea66caa1374066
Paul Michael John Lopez <pmjlopez@gmail.com>
2018-09-04 13:43:33 +0800

    refactor: clean up

--- src/components/App.js
+++ src/components/App.js
@@ -2,39 +2,63 @@ import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { handleInitialData } from "../actions/shared";
+import { handleReceiveUsers } from "../actions/users";
 import LoadingBar from 'react-redux-loading'
 import Nav from './Nav'
 import Dashboard from './Dashboard'
 import QuestionPage from './QuestionPage'
+import Login from './Login'
+import NewQuestion from './NewQuestion'
+import LeaderBoard from './LeaderBoard'
 
 class App extends Component {
     componentDidMount() {
-        this.props.dispatch(handleInitialData())
+        this.props.authedUser
+            ? this.props.dispatch(handleInitialData(this.props.authedUser))
+            : this.props.dispatch(handleReceiveUsers())
+    }
+    componentDidUpdate(prevProps) {
+        if (this.props.authedUser !== prevProps.authedUser ) {
+            this.props.dispatch(handleInitialData(this.props.authedUser))
+        }
     }
     render() {
         return (
             <Router>
                 <Fragment>
-                    <LoadingBar/>
-                    <div className='container'>
-                        <Nav/>
-                        {this.props.loading === true
-                            ? null
-                            : <div>
-                                <Route path='/' exact component={Dashboard}/>
-                                <Route path='/questions/:id' component={QuestionPage}/>
+                    {this.props.authedUser && (
+                        <Fragment>
+                            <Nav/>
+                            <LoadingBar/>
+                            <div className='container'>
+                                {this.props.loading === true
+                                    ? null
+                                    : <div>
+                                        <Route path='/' exact component={Dashboard}/>
+                                        <Route path='/questions/:id' component={QuestionPage}/>
+                                        <Route path='/new' component={NewQuestion}/>
+                                        <Route path='/leader-board' component={LeaderBoard}/>
+                                    </div>
+                                }
                             </div>
-                        }
-                    </div>
+                        </Fragment>
+                    )}
+                    {!this.props.authedUser && (
+                        <Fragment>
+                            <LoadingBar/>
+                            <Login/>
+                        </Fragment>
+                    )}
                 </Fragment>
             </Router>
         );
     }
 }
 
-function mapStateToProps ({ authedUser }) {
+function mapStateToProps ({ authedUser, users }) {
     return {
-        loading: authedUser === null
+        loading: authedUser === null,
+        authedUser
     }
 }
 
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,31 +2,83 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Question from './Question'
 
+const UNANSWERED_QUESTIONS = 'UNANSWERED_QUESTIONS'
+const ANSWERED_QUESTIONS = 'ANSWERED_QUESTIONS'
+
 class Dashboard extends Component {
+    state = {
+        questionIds: [],
+        tab: ''
+    }
+    componentDidMount () {
+        this.setState(() => ({
+            tab: UNANSWERED_QUESTIONS
+        }))
+    }
+    componentDidUpdate (prevProps,prevState) {
+        if (this.state.tab !== prevState.tab
+            || this.props.questionIds !== prevProps.questionIds
+            || this.props.user.answers !== prevProps.user.answers
+        ) {
+            this.handleFilterQuestions(this.state.tab)
+        }
+    }
+    getFilteredQuestions = (selectedTab) => {
+        const { user, questionIds } = this.props
+        const answerIds = Object.keys(user.answers)
+        const qIds = selectedTab === UNANSWERED_QUESTIONS
+            ? questionIds.filter((id) => !answerIds.includes(id))
+            : questionIds.filter((id) => answerIds.includes(id))
+        return qIds
+    }
+    handleFilterQuestions = (selectedTab) => {
+        console.log('Selected Tab', selectedTab)
+        const qIds = this.getFilteredQuestions(selectedTab)
+        this.setState((prevState) => ({
+            questionIds: qIds,
+            tab: selectedTab
+        }))
+    }
     render() {
+        console.log('Question IDs', this.state.questionIds)
         return (
-            <div>
+            <div className='container'>
                 <h3 className='center'>
                     Would You Rather
                 </h3>
-                <ul>
-                    <li><a>Unanswered Questions</a></li>
-                    <li><a>Answered Questions</a></li>
+                <ul className='nav nav-tabs'>
+                    <li className='nav-item'>
+                        <button
+                            onClick={() => this.handleFilterQuestions(UNANSWERED_QUESTIONS)}
+                            className={`nav-link ${this.state.tab === UNANSWERED_QUESTIONS ? 'active' : ''}`}
+                        >
+                            Unanswered Questions
+                        </button>
+                    </li>
+                    <li className='nav-item'>
+                        <button
+                            onClick={() => this.handleFilterQuestions(ANSWERED_QUESTIONS)}
+                            className={`nav-link ${this.state.tab === ANSWERED_QUESTIONS ? 'active' : ''}`}
+                        >
+                            Answered Questions
+                        </button>
+                    </li>
                 </ul>
-                <ul className='card-list'>
-                    {this.props.questionIds.map((id) => (
-                        <li key={id}>
+                <div className='row'>
+                    {this.state.questionIds.map((id) => (
+                        <div key={id} className='col-lg-4'>
                             <Question id={id}/>
-                        </li>
+                        </div>
                     ))}
-                </ul>
+                </div>
             </div>
         );
     }
 }
 
-function mapStateToProps ({ questions }) {
+function mapStateToProps ({ questions, users, authedUser }) {
     return {
+        user: users[authedUser],
         questionIds: Object.keys(questions)
             .sort((a,b) => questions[b].timestamp - questions[a].timestamp)
     }
