bf8e01a035ddecb34993a69d7945a4413cc4b2b6
Paul Michael John Lopez <pmjlopez@gmail.com>
2018-09-12 15:05:47 +0800

    refactor: remove unused requests to api

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -1,46 +1,15 @@
-import { saveAuthedUser, deleteAuthedUser } from "../utils/api"
-import { showLoading, hideLoading } from 'react-redux-loading'
-
 export const SET_AUTHED_USER = 'SET_AUTHED_USER'
 export const UNSET_AUTHED_USER = 'UNSET_AUTHED_USER'
 
-function setAuthedUser (id) {
+export function setAuthedUser (id) {
     return {
         type: SET_AUTHED_USER,
         id
     }
 }
 
-export function handleSetAuthedUser (id) {
-    return (dispatch) => {
-        dispatch(showLoading())
-        return saveAuthedUser(id)
-            .then(() => dispatch(setAuthedUser(id)))
-            .then(() => dispatch(hideLoading()))
-            .catch((e) => {
-                console.warn(`Error in handleSetAutheduser: `, e)
-                alert(`There was an error in setting authenticated user. Try again.`)
-                dispatch(hideLoading())
-            })
-    }
-}
-
-function unsetAuthedUser () {
+export function unsetAuthedUser () {
     return {
         type: UNSET_AUTHED_USER
-    }
-}
-
-export function handleUnsetAuthedUser () {
-    return (dispatch) => {
-        dispatch(showLoading())
-        return deleteAuthedUser()
-            .then(() => dispatch(unsetAuthedUser()))
-            .then(() => dispatch(hideLoading()))
-            .catch((e) => {
-                console.warn(`Error in handleUnsetAutheduser: `, e)
-                alert(`There was an error in deleting authenticated user. Try again.`)
-                dispatch(hideLoading())
-            })
     }
 }
