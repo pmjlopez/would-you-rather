d1242a26cfa5ff0263ef07f6e931b8e373994b5f
Paul Michael John Lopez <pmjlopez@gmail.com>
2018-09-13 18:55:59 +0800

    feat: show loading text when loading select options

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,13 +12,20 @@ class Login extends Component {
         const { userIds, users } = this.props
         return (
             <div className='container text-center mt-4'>
-                <h5>Login</h5>
-                <select onChange={this.handleChangeUser}>
-                    <option value=''>Select User</option>
-                    {userIds.map((id) => (
-                        <option key={id} value={id}>{users[id].name}</option>
-                    ))}
-                </select>
+                {userIds.length > 0 && (
+                    <div>
+                        <h5>Login</h5>
+                        <select onChange={this.handleChangeUser}>
+                        <option value=''>Select User</option>
+                        {userIds.map((id) => (
+                            <option key={id} value={id}>{users[id].name}</option>
+                        ))}
+                        </select>
+                    </div>
+                )}
+                {!(userIds.length > 0) && (
+                    <h5>Loading</h5>
+                )}
             </div>
         )
     }
